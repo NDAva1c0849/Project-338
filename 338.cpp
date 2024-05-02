0ee0bbdd51ff1f3195af995322e9b6276e7aa7d6
NDAva1c0849 <109666538+NDAva1c0849@users.noreply.github.com>
2024-05-02 11:57:45 +0800

    Add files via upload

---
+++ 338.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	system("color F6");
+	cout << "Copyright 治联科技 2024.All Rights Reserved." << endl;
+	cout << "请输入密码：";
+	int k,a=0;
+	cin >> k;
+	switch (k)
+	{
+	case 38:
+		a = 1;
+		while(a){
+			cout << "缺席" << endl;
+		}
+		break;
+	case 338:
+		a = 1;
+		while (a) {
+			cout << "SMAR???"<<endl;
+		}
+		break;
+	case 338338:
+		a = 1;
+		while (a) {
+			cout << "怠批"<<endl;
+		}
+		break;
+	case 52338:
+		a = 1;
+		while (a) {
+			cout << "嗯~么" << endl;
+		}
+		break;
+	default:
+		a = 1;
+		while (a) {
+			cout << "roll away!!!"<<endl;
+		};
+		break;
+	}
+	system("pause");
+	return 0;
+}
---
+++ roll a0.3.8.vbs
@@ -0,0 +1,20 @@
+dim c,k
+k="338"
+c=inputbox("输入密码以获得永久访问博轩的权限")
+if c=338 then
+msgbox "缺席"
+end if 
+if c=338338 then
+msgbox"啊？？？？？"
+end if
+if c=3838338 then
+msgbox "给你整上画面了！"
+end if 
+if c=52338 then
+msgbox "嗯~么"
+end if 
+if c=338448 then
+msgbox"SMAR???"
+else
+msgbox "roll you again"
+end if
